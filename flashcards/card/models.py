1884e8e0c44e66f2ce168c53c7d21989db85bfdd
JIVE <116755384+JIVERU@users.noreply.github.com>
2025-10-26 23:57:52 +0800

    Defined card scheduler methods

--- flashcards/card/models.py
+++ flashcards/card/models.py
@@ -2,32 +2,46 @@ from django.db import models
 from django.utils import timezone
 from deck.models import Deck
 from user.models import User
-
+from fsrs import Card as FSRS_Card, State as FSRS_State
+from card.scheduler import Scheduler
 
 class Card(models.Model):
     class State(models.IntegerChoices):
-        NEW = 0
-        LEARNING = 1
-        REVIEW = 2
-        RELEARNING = 3
+        Learning = 1
+        Review = 2
+        Relearning = 3
 
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='cards')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     stability = models.FloatField(default=0)
     difficulty = models.FloatField(default=0)
-    state = models.IntegerField(choices=State.choices, default=State.NEW)
+    state = models.IntegerField(choices=State.choices, default=State.Learning)
     last_review = models.DateTimeField(null=True, blank=True)
     next_review = models.DateTimeField(default=timezone.now)
     step = models.IntegerField(null=True, blank=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Card {self.id} (Due:{self.next_review:.2f})"
+    def update_schedule(self, rating):
+        FSRSCard = FSRS_Card(
+            state=FSRS_State(self.state),
+            stability=self.stability,
+            difficulty=self.difficulty,
+            step=self.step,
+            due=self.next_review,
+            last_review=self.last_review
+        )
 
-    # Placeholder method to change the schedule based on a spaced-repetition algorithm
-    def update_schedule(self, performance_rating):
-        pass
+        scheduler = Scheduler()
+        FSRSCard_new, reviewLog = scheduler.review_card(FSRSCard, rating)
+
+        self.state = FSRSCard_new.state
+        self.stability = FSRSCard_new.stability
+        self.difficulty = FSRSCard_new.difficulty
+        self.step = FSRSCard_new.step
+        self.next_review = FSRSCard_new.due
+        self.last_review = FSRSCard_new.last_review
+        self.save()
 
 class BasicCard(Card):
     front_field = models.TextField()
@@ -37,4 +51,4 @@ class IdentificationCard(Card):
     hidden_field = models.TextField()
 
 class ImageOcclusionCard(Card):
-    img_path = models.ImageField(upload_to='occlusion_images/')
+    img_path = models.ImageField(upload_to='static/card/occlusion_images/')
