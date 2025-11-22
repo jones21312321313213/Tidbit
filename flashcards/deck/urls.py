a0f7bd58b1acce9792805e541948202859fa6cbb
HPCS <saysonmichael11@gmail.com>
2025-11-22 10:51:57 +0800

    added html and modified urls.py and views.py

--- flashcards/deck/urls.py
+++ flashcards/deck/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('deck/', views.DeckListView.as_view(), name='deck_list'),
+    path('', views.DeckListView.as_view(), name='deck_list'),
+    path('create/', views.DeckCreateView.as_view(), name='deck_create'),
     path('<slug:slug>/', views.DeckUpdateView.as_view(), name='deck_detail'),
 ]
