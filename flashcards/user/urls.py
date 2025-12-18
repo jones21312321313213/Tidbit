4e57c8005c1636dde2b032103e1d1919d60850aa
jones21312321313213 <skinnyjones212121@gmail.com>
2025-12-18 19:53:19 +0800

    added forgot password

--- flashcards/user/urls.py
+++ flashcards/user/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -17,6 +20,11 @@ urlpatterns = [
     path('forgot-password/code', views.ForgotPasswordCodeView.as_view(), name='forgot_password_code'),
     path('forgot-password/changing-password', views.ForgotPasswordChangePasswordView.as_view(), name='forgot_password_changepw'),
     path('forgot-password/success', views.ForgotPasswordSuccessView.as_view(), name='forgot_password_success'),
-    
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="user/forgot_password.html", success_url='/reset_password_sent/'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "user/forgot_password_code.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.ForgotPasswordChangePasswordView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/forgot_password_success.html'), name='password_reset_complete'),
+
     path("folder/", include("folder.urls")),
 ]
--- flashcards/user/views.py
+++ flashcards/user/views.py
@@ -4,18 +4,18 @@ from django.views import View
 from django.contrib import messages
 from django.urls import reverse
 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout, update_session_auth_hash, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from django.utils.decorators import method_decorator
-
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordResetConfirmView
 
 from .forms import UserForm, CreateUserForm, ChangeEmailForm
+from .procedures import insert_user_user, loginUser, update_user_email,verify_user_password,update_userPassword,get_userInfo
+from .mixins import LoginRequiredMessageMixin
+
+
 from folder.models import Folder
-from .procedures import insert_user_user, loginUser, update_user_email,verify_user_password
-
-
 # Create your views here.
 
 
@@ -36,30 +36,28 @@ class LoginView(View):
         password = request.POST.get("password")
         next_url = request.POST.get('next') or request.GET.get('next')  # <-- read the next parameter
 
-        # user = authenticate(request, username=username, password=password)
-        #
-        # if user is not None:
-        #     login(request, user)
-        #     return redirect('home')   # home page
-        # else:
-        #     messages.error(request, "Username or password is incorrect")
-        #     return redirect('login')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')   # home page
+        else:
+            messages.error(request, "Username or password is incorrect")
+            return redirect('login')
 
         #for this to work u need to create loginUser in ur mysql which can be found on the stored_procedure dir
-        if loginUser(username, password):
-            # request.session['username'] = username
-            #need this for user to be authenticated using Django so that we can go to next views
-            #because we are using LoginRequiredMixin
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                # optionally create a Django user if it doesn't exist
-                user = User.objects.create_user(username=username, password=password)
-            login(request, user)  # <-- attaches user to session
-            return redirect(next_url or 'home')
-        else:
-            #message = "Invalid username or password"
-            messages.error(request, "Invalid username or password")
+        # if loginUser(username, password):
+        #     # request.session['username'] = username
+        #     #need this for user to be authenticated using Django so that we can go to next views
+        #     #because we are using LoginRequiredMixin
+        #
+        #
+        #     user = User.objects.get(username=username)
+        #     login(request, user)  # <-- attaches user to session
+        #     return redirect(next_url or 'home')
+        # else:
+        #     #message = "Invalid username or password"
+        #     messages.error(request, "Invalid username or password")
 
         return render(request, self.template_name)
 
@@ -75,11 +73,19 @@ class ForgotPasswordCodeView(View):
     def get(self,request):
         return render(request, self.template_name)
 
-class ForgotPasswordChangePasswordView(View):
+class ForgotPasswordChangePasswordView(PasswordResetConfirmView):
     template_name = 'user/forgot_password_changepw.html'
+    success_url = "/forgot-password/success"
 
-    def get(self,request):
-        return render(request, self.template_name)
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # user whose password was just reset
+        username = self.user.username
+
+        update_userPassword(username)
+
+        return response
 
 class ForgotPasswordSuccessView(View):
     template_name = 'user/forgot_password_success.html'
@@ -105,7 +111,7 @@ def registerPage(request):
             # redirect to login page with username password already in the inputs
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-            login_url = f"{reverse('login')}?username={username}&password={password}"
+            login_url = f"{reverse('login')}?username={username}&password={password}" #para auto butang user & pass sa input fields sa login page
             return redirect(login_url)
 
     context = {'form': form}
@@ -114,22 +120,25 @@ def registerPage(request):
 
 
 # PROFILE Management
-class ProfileView(LoginRequiredMixin,View):
+class ProfileView(LoginRequiredMessageMixin,View):
     login_url = 'login'
     template_name = 'user/profile.html'
     def get(self, request):
-        user =request.user
+
+        user = get_userInfo(request.user.username)
 
         context = {
-            'username': user.username,
-            'email': user.email,
-            'created_at': user.date_joined,
+            'userId': user["userId"],
+            'username': user["username"],
+            'email': user["email"],
+            'created_at': user["created_at"],
         }
         return render(request, self.template_name,context)
 
-class ChangeEmailView(LoginRequiredMixin,View):
+class ChangeEmailView(LoginRequiredMessageMixin,View):
     login_url = 'login'
     template_name = 'user/change_email.html'
+
 
     def get(self,request):
         # email = User.objects.get(username=request.session['username'])
@@ -137,7 +146,13 @@ class ChangeEmailView(LoginRequiredMixin,View):
         user = request.user
         initial_data = {'email': user.email}
         form = ChangeEmailForm(initial=initial_data)
-        return render(request, self.template_name, {'form': form})
+
+        context = {
+            'form': form,
+            'userId': get_userInfo(user.username)["userId"],
+        }
+
+        return render(request, self.template_name, context)
 
     def post(self,request):
         # email = User.objects.get(username=request.session['username'])
@@ -146,7 +161,7 @@ class ChangeEmailView(LoginRequiredMixin,View):
         user = request.user
         form = ChangeEmailForm(request.POST)
         current_password = request.POST.get('current_password')
-        error_password = None  # placeholder for password errors
+
 
         if not current_password or not verify_user_password(user.username, current_password):
             error_password = "Current password is incorrect"
@@ -165,16 +180,35 @@ class ChangeEmailView(LoginRequiredMixin,View):
         return render(request, self.template_name,{'form': form,})
 
 
-class ChangePasswordView(LoginRequiredMixin,View):
-    login_url =  'login'
+class ChangePasswordView(LoginRequiredMessageMixin, View):
+    login_url = 'login'
     template_name = 'user/change_password.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        form = PasswordChangeForm(request.user)
+        context = {
+            'form': form,
+            'userId': get_userInfo(request.user.username)["userId"],
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()  # updates auth_user.password
+            update_session_auth_hash(request, user)
+
+            update_userPassword(user.username) #gets the updated password from auth_user table and put its in user_user tbl
+
+            messages.success(request, "Password successfully updated")
+            return redirect('profile')
+
+        messages.error(request, "Please fix the errors below")
+        return render(request, self.template_name, {'form': form})
 
 
 #LoginRequiredMixin does not allow user to go to that page without being logged in
-class HomePageView(LoginRequiredMixin,View):
+class HomePageView(LoginRequiredMessageMixin,View):
     login_url = 'login'
     template_name = 'user/home.html'
 
