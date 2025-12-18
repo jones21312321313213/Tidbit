3da18b8944446c2e582fce9d0cdcccaf2e26b958
JMAC137 <51169542+JonMariC137@users.noreply.github.com>
2025-12-19 00:36:47 +0800

    removed unnecessary notifs url

--- flashcards/notification/urls.py
+++ flashcards/notification/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     # Main notification list view
     path('', views.NotificationListView.as_view(), name='notification_list'),
-    path('<int:pk>/read/', views.NotificationMarkAsReadView.as_view(), name='notification_read'),
-
 ]
