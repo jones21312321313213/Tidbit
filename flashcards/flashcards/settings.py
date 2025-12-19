041cf442bc63abe7d2e924dbfda42ff46be584d9
JMAC137 <51169542+JonMariC137@users.noreply.github.com>
2025-12-19 09:55:45 +0800

    Update settings.py

--- flashcards/flashcards/settings.py
+++ flashcards/flashcards/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbflashcards',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
